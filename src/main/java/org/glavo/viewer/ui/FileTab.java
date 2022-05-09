df1fa0c24e0c880a7a7990a548b91289bff92636
Glavo <zjx001202@gmail.com>
2022-05-09 19:50:53 +0800

    Add side bar to file type

--- src/main/java/org/glavo/viewer/ui/FileTab.java
+++ src/main/java/org/glavo/viewer/ui/FileTab.java
@@ -1,5 +1,8 @@
 package org.glavo.viewer.ui;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.scene.Node;
 import javafx.scene.control.Tab;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.ImageView;
@@ -9,6 +12,8 @@ import org.glavo.viewer.file.FileType;
 public class FileTab extends Tab {
     private final FileType type;
     private final FilePath path;
+
+    private final ObjectProperty<Node> sideBar = new SimpleObjectProperty<>();
 
     public FileTab(FileType type, FilePath path) {
         this.type = type;
@@ -25,5 +30,17 @@ public class FileTab extends Tab {
 
     public FilePath getPath() {
         return path;
+    }
+
+    public ObjectProperty<Node> sideBarProperty() {
+        return sideBar;
+    }
+
+    public Node getSideBar() {
+        return sideBar.get();
+    }
+
+    public void setSideBar(Node sideBar) {
+        this.sideBar.set(sideBar);
     }
 }
