3e25c8e27a6a23704db42389c53037315d71e26c
Glavo <zjx001202@gmail.com>
2022-05-16 13:58:32 +0800

    git add -A

--- src/main/java/org/glavo/viewer/ui/FallbackHexPane.java
+++ src/main/java/org/glavo/viewer/ui/FallbackHexPane.java
@@ -14,17 +14,22 @@ public class FallbackHexPane extends StackPane implements HexPane {
 
         area.getStylesheets().clear();
         area.setParagraphGraphicFactory(LineNumberFactory.get(area, i -> "%08X"));
-        //area.setEditable(false);
+        area.setEditable(false);
         String text = new HexText(array).bytesText;
         area.replaceText(text);area.scrollToPixel(0, 0);
         VirtualizedScrollPane<CodeArea> scrollPane = new VirtualizedScrollPane<>(area);
         this.getChildren().add(scrollPane);
-
-
     }
 
     @Override
     public void select(int offset, int length) {
-        // TODO
+        area.selectRange(calcBytesTextPosition(offset), calcBytesTextPosition(offset + length) - 1);
+    }
+
+    private static int calcBytesTextPosition(int byteOffset) {
+        int rowIndex = byteOffset / HexText.BYTES_PER_ROW;
+        int colIndex = byteOffset % HexText.BYTES_PER_ROW;
+
+        return (49 * rowIndex) + (colIndex * 3);
     }
 }
