82f09a636a1b550fb5efde4637f4242cdaccd3d1
Glavo <zjx001202@gmail.com>
2022-05-21 17:44:51 +0800

    JVM constants

---
+++ src/main/java/org/glavo/viewer/file/types/java/classfile/jvm/AccessFlagType.java
@@ -0,0 +1,12 @@
+package org.glavo.viewer.file.types.java.classfile.jvm;
+
+//@formatter:off
+public class AccessFlagType {
+
+    public static final int AF_CLASS        = 0b00001;
+    public static final int AF_FIELD        = 0b00010;
+    public static final int AF_METHOD       = 0b00100;
+    public static final int AF_NESTED_CLASS = 0b01000;
+    public static final int AF_MODULE_ATTR  = 0b10000;
+    public static final int AF_ALL          = 0b11111;
+}
