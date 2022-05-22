6cad3784a3788893d7adc080f0409da34cd19b9a
Glavo <zjx001202@gmail.com>
2022-05-23 00:51:11 +0800

    JavaTypes

--- src/main/java/org/glavo/viewer/file/types/java/classfile/jvm/type/ArrayType.java
+++ src/main/java/org/glavo/viewer/file/types/java/classfile/jvm/type/ArrayType.java
@@ -2,13 +2,19 @@ package org.glavo.viewer.file.types.java.classfile.jvm.type;
 
 public final class ArrayType extends JavaType {
     private final JavaType baseType;
+    private final int level;
 
-    ArrayType(JavaType baseType, String qualified, String descriptor) {
+    ArrayType(JavaType baseType, int level, String qualified, String descriptor) {
         super(qualified, descriptor);
         this.baseType = baseType;
+        this.level = level;
     }
 
     public JavaType getBaseType() {
         return baseType;
+    }
+
+    public int getLevel() {
+        return level;
     }
 }
--- src/main/java/org/glavo/viewer/file/types/java/classfile/jvm/type/JavaTypes.java
+++ src/main/java/org/glavo/viewer/file/types/java/classfile/jvm/type/JavaTypes.java
@@ -1,8 +1,15 @@
 package org.glavo.viewer.file.types.java.classfile.jvm.type;
 
+import kala.value.primitive.IntRef;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public abstract class JavaTypes {
     private JavaTypes() {
     }
+
+    public static final PrimitiveType VOID = new PrimitiveType("void", "V");
 
     public static final PrimitiveType BYTE = new PrimitiveType("byte", "B");
     public static final PrimitiveType CHAR = new PrimitiveType("char", "C");
@@ -13,4 +20,74 @@ public abstract class JavaTypes {
     public static final PrimitiveType SHORT = new PrimitiveType("short", "S");
     public static final PrimitiveType BOOLEAN = new PrimitiveType("boolean", "Z");
 
+    public static ArrayType newArrayType(JavaType baseType, int level) {
+        assert level >= 1;
+
+        String qualified = baseType.getQualified() + "[]".repeat(level);
+        String descriptor = "[".repeat(level) + baseType.getDescriptor();
+
+        return new ArrayType(baseType, level, qualified, descriptor);
+    }
+
+    public static MethodType newMethodType(JavaType returnType, List<JavaType> parameterTypes) {
+        String qualified = parameterTypes.stream().map(JavaType::getQualified).collect(Collectors.joining(
+                ", ", "(", "): " + returnType.getQualified()
+        ));
+
+        String descriptor = parameterTypes.stream().map(JavaType::getDescriptor).collect(Collectors.joining(
+                "", "(", ")" + returnType.getDescriptor()
+        ));
+
+        return new MethodType(returnType, parameterTypes, qualified, descriptor);
+    }
+
+    public static JavaType parseDescriptor(String descriptor) {
+        if (descriptor == null || descriptor.isEmpty()) throw new IllegalArgumentException("descriptor is empty");
+
+        IntRef length = new IntRef();
+
+        if (descriptor.charAt(0) == '(') {
+            // TODO
+        } else {
+            JavaType type = scanDescriptor(descriptor, 0, length);
+            if (length.value == descriptor.length()) return type;
+        }
+
+        throw new IllegalArgumentException("malformed descriptor '" + descriptor + "'");
+    }
+
+    public static JavaType scanDescriptor(String descriptor, int begin, IntRef length) {
+        int len = descriptor.length() - begin;
+        if (len == 0) throw new IllegalArgumentException("descriptor is empty");
+
+        switch (descriptor.charAt(0)) {
+            case 'B':
+                return BYTE;
+            case 'C':
+                return CHAR;
+            case 'D':
+                return DOUBLE;
+            case 'F':
+                return FLOAT;
+            case 'I':
+                return INT;
+            case 'J':
+                return LONG;
+            case 'S':
+                return SHORT;
+            case 'Z':
+                return BOOLEAN;
+            case 'L':
+                int idx = descriptor.indexOf(';', begin + 1);
+                if (idx >= 0) {
+
+                }
+                break;
+            case '[':
+                // TODO
+                break;
+        }
+
+        throw new IllegalArgumentException("malformed descriptor '" + descriptor + "'");
+    }
 }
