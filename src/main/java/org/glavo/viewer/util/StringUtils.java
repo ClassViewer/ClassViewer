6b338ea691253f79e55cfc31fcf5b53adaf72418
Glavo <zjx001202@gmail.com>
2022-06-06 01:49:46 +0800

    Optimization StringUtils.formatIndex

--- src/main/java/org/glavo/viewer/util/StringUtils.java
+++ src/main/java/org/glavo/viewer/util/StringUtils.java
@@ -59,7 +59,37 @@ public final class StringUtils {
         }
     }
 
+    private static final String[] level1 = {"#0", "#1", "#2", "#3", "#4", "#5", "#6", "#7", "#8", "#9"};
+    private static final String[] level2 = {
+            "#00", "#01", "#02", "#03", "#04", "#05", "#06", "#07", "#08", "#09",
+            "#10", "#11", "#12", "#13", "#14", "#15", "#16", "#17", "#18", "#19",
+            "#20", "#21", "#22", "#23", "#24", "#25", "#26", "#27", "#28", "#29",
+            "#30", "#31", "#32", "#33", "#34", "#35", "#36", "#37", "#38", "#39",
+            "#40", "#41", "#42", "#43", "#44", "#45", "#46", "#47", "#48", "#49",
+            "#50", "#51", "#52", "#53", "#54", "#55", "#56", "#57", "#58", "#59",
+            "#60", "#61", "#62", "#63", "#64", "#65", "#66", "#67", "#68", "#69",
+            "#70", "#71", "#72", "#73", "#74", "#75", "#76", "#77", "#78", "#79",
+            "#80", "#81", "#82", "#83", "#84", "#85", "#86", "#87", "#88", "#89",
+            "#90", "#91", "#92", "#93", "#94", "#95", "#96", "#97", "#98", "#99"
+    };
+    private static final String[] level3 = new String[1000];
+
     public static String formatIndex(int index, int maxIndex) {
+        if (maxIndex < 10) {
+            return level1[index];
+        }
+
+        if (maxIndex < 100) {
+            return level2[index];
+        }
+
+        if (maxIndex < 1000) {
+            String res = level3[index];
+            if (res == null)
+                res = level3[index] = String.format("#%03d", index).intern();
+            return res;
+        }
+
         int idxWidth = String.valueOf(maxIndex).length();
         String fmtStr = "#%0" + idxWidth + "d";
         return String.format(fmtStr, index);
