f556f75139808232fcd36c4054bc0205a751f0c2
Glavo <zjx001202@gmail.com>
2024-04-25 05:11:46 +0800

    Create ImageInfoTable

--- src/main/java/org/glavo/viewer/file/types/image/ImageFileType.java
+++ src/main/java/org/glavo/viewer/file/types/image/ImageFileType.java
@@ -15,12 +15,18 @@
  */
 package org.glavo.viewer.file.types.image;
 
+import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressIndicator;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
+import javafx.util.Pair;
 import kala.function.CheckedSupplier;
+import org.apache.commons.imaging.ImageFormats;
+import org.apache.commons.imaging.ImageInfo;
 import org.glavo.viewer.file.Container;
 import org.glavo.viewer.file.CustomFileType;
 import org.glavo.viewer.file.FileHandle;
@@ -80,5 +86,45 @@ public final class ImageFileType extends CustomFileType {
         }, Schedulers.javafx());
 
         return tab;
+    }
+
+    private static final class ImageInfoTable extends TableView<Pair<String, String>> {
+        ImageInfoTable(ImageInfo info) {
+            this.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY_FLEX_LAST_COLUMN);
+            TableColumn<Pair<String, String>, String> attributeName = new TableColumn<>(I18N.getString("attribute.name"));
+            TableColumn<Pair<String, String>, String> attributeValue = new TableColumn<>(I18N.getString("attribute.value"));
+
+            attributeName.setCellValueFactory(f -> new ReadOnlyStringWrapper(f.getValue().getKey()));
+            attributeValue.setCellValueFactory(f -> new ReadOnlyStringWrapper(f.getValue().getValue()));
+
+            getColumns().add(attributeName);
+            getColumns().add(attributeValue);
+
+            getItems().add(new Pair<>(I18N.getString("image.attributes.format"), info.getFormat().getName()));
+            getItems().add(new Pair<>(I18N.getString("image.attributes.width"), String.valueOf(info.getWidth())));
+            getItems().add(new Pair<>(I18N.getString("image.attributes.height"), String.valueOf(info.getHeight())));
+            getItems().add(new Pair<>(I18N.getString("image.attributes.bitsPerPixel"), String.valueOf(info.getBitsPerPixel())));
+            getItems().add(new Pair<>(I18N.getString("image.attributes.colorType"), info.getColorType().toString()));
+            getItems().add(new Pair<>(I18N.getString("image.attributes.compressionAlgorithm"), info.getCompressionAlgorithm().toString()));
+
+            if (info.getFormat() == ImageFormats.GIF || info.getFormat() == ImageFormats.TIFF) {
+                getItems().add(new Pair<>(I18N.getString("image.attributes.numberOfImages"), String.valueOf(info.getNumberOfImages())));
+            }
+
+            if (info.getFormat() == ImageFormats.TIFF
+                    || info.getFormat() == ImageFormats.BMP
+                    || info.getFormat() == ImageFormats.JPEG
+                    || info.getFormat() == ImageFormats.PNG) {
+                if (info.getPhysicalWidthInch() >= 0)
+                    getItems().add(new Pair<>(I18N.getString("image.attributes.physicalWidthInch"), String.valueOf(info.getPhysicalWidthInch())));
+                if (info.getPhysicalHeightInch() >= 0)
+                    getItems().add(new Pair<>(I18N.getString("image.attributes.physicalHeightInch"), String.valueOf(info.getPhysicalHeightInch())));
+
+                if (info.getPhysicalWidthDpi() >= 0)
+                    getItems().add(new Pair<>(I18N.getString("image.attributes.physicalWidthDpi"), String.valueOf(info.getPhysicalWidthDpi())));
+                if (info.getPhysicalHeightDpi() >= 0)
+                    getItems().add(new Pair<>(I18N.getString("image.attributes.physicalHeightDpi"), String.valueOf(info.getPhysicalHeightDpi())));
+            }
+        }
     }
 }
