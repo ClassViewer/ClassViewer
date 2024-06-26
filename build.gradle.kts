8e9275c7d3465b01b95ecb84b09b796fcbd423af
Glavo <zjx001202@gmail.com>
2024-06-26 19:47:53 +0800

    Bump richtextfx to 0.11.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -75,7 +75,7 @@ dependencies {
     implementation("org.hildan.fxgson:fx-gson:5.0.0")
 
     // https://mvnrepository.com/artifact/org.fxmisc.richtext/richtextfx
-    implementation("org.fxmisc.richtext:richtextfx:0.11.2")
+    implementation("org.fxmisc.richtext:richtextfx:0.11.3")
 
     // https://mvnrepository.com/artifact/org.glavo/chardet
     implementation("org.glavo:chardet:2.4.0-beta1")
