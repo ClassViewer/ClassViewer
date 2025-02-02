d3c483134e24a0e8f6ce3e4fd34e5b99872b33d3
Glavo <zjx001202@gmail.com>
2025-02-02 09:24:14 +0800

    Fix build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,6 @@
 plugins {
     java
     application
-    id("org.openjfx.javafxplugin") version "0.0.10"
-    id("org.beryx.jlink") version "2.24.1"
-    id("com.gluonhq.gluonfx-gradle-plugin") version "1.0.4"
     id("org.glavo.compile-module-info-plugin") version "2.0"
 }
 
@@ -18,48 +15,20 @@ repositories {
 }
 
 application {
-    mainClass.set("$viewerModuleName/$viewerMainClassName")
+    mainClass.set(viewerMainClassName)
 }
 
-javafx {
-    version = "21"
-    modules("javafx.controls")
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 tasks.compileJava {
-    options.release.set(8)
-    options.javaModuleMainClass.set(viewerMainClassName)
     options.encoding = "UTF-8"
 }
 
 tasks.jar {
     manifest.attributes(
-        "Implementation-Version" to "1.2",
         "Main-Class" to viewerMainClassName
     )
 }
-
-gluonfx {
-    bundlesList = listOf("org.glavo.viewer.gui.ViewerResources")
-    reflectionList = listOf("org.glavo.viewer.gui.Viewer", "javafx.scene.input.Dragboard")
-    jniList = listOf("com.sun.glass.ui.CommonDialogs")
-}
-
-//jlink --strip-debug --no-header-files --no-man-pages --module-path ClassViewer-3.x.jar --add-modules org.glavo.viewer --output ClassViewer --strip-native-commands --vm=client
-//jlink {
-//    moduleName.set(viewerModuleName)
-//    imageDir.set(file("$buildDir/ClassViewer"))
-//    imageZip.set(file("$buildDir/ClassViewer-$version.zip"))
-//
-//    addOptions(
-//        "--strip-debug",
-//        "--no-header-files",
-//        "--no-man-pages",
-//        "--strip-native-commands"
-//    )
-//
-//    project.tasks.getByName("jlink").doLast {
-//        delete("$imageDir/bin/ClassViewer")
-//        delete("$imageDir/bin/ClassViewer.bat")
-//    }
-//}
