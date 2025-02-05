dc77903dbe88455f5de04f7c67b3be9783c7ac4f
Glavo <zjx001202@gmail.com>
2025-02-05 11:52:29 +0800

    Create launcher

---
+++ launcher/launcher.cpp
@@ -0,0 +1,64 @@
+//
+// Created by Glavo on 2020.1.8.
+//
+
+#include "launcher.h"
+#include <windows.h>
+#include <shlwapi.h>
+#include <pathcch.h>
+#include <string>
+#include <vector>
+
+#define MAX_LONG_PATH_SIZE 32768
+
+#ifdef APPLICATION_SHOW_CONSOLE
+int main() {
+    LPWSTR pCmdLine = PathGetArgsW(GetCommandLineW());
+#else
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
+#endif
+    auto appName = static_cast<LPWSTR>(std::calloc(sizeof(WCHAR), MAX_LONG_PATH_SIZE));
+
+    if (!GetModuleFileNameW(nullptr, appName, MAX_LONG_PATH_SIZE)) {
+        MessageBoxW(nullptr, L"Failed to get module file name.", nullptr, MB_OK);
+        return EXIT_FAILURE;
+    }
+
+    PathCchRemoveFileSpec(appName, MAX_LONG_PATH_SIZE);
+    PathCchAppendEx(appName, MAX_LONG_PATH_SIZE, APPLICATION_JRE_PATH "\\bin\\java.exe",
+                    PATHCCH_ALLOW_LONG_PATHS);
+
+    std::wstring cmdLine;
+    cmdLine += L'"';
+    cmdLine += appName;
+    cmdLine += L'"';
+
+    free(appName);
+
+    cmdLine += L" -m " APPLICATION_MAIN_MODULE;
+
+    if (wcslen(pCmdLine) > 0) {
+        cmdLine += L' ';
+        cmdLine += pCmdLine;
+    }
+
+    STARTUPINFOW si;
+    PROCESS_INFORMATION pi;
+    si.cb = sizeof(si);
+    ZeroMemory(&si, sizeof(si));
+    ZeroMemory(&pi, sizeof(pi));
+
+    return CreateProcessW(nullptr,
+                          &cmdLine[0],
+                          nullptr,
+                          nullptr,
+                          false,
+                          NORMAL_PRIORITY_CLASS,
+                          nullptr,
+                          nullptr,
+                          &si,
+                          &pi);
+}
+
+
---
+++ launcher/launcher.h
@@ -0,0 +1,11 @@
+//
+// Created by Glavo on 2019.12.24.
+//
+
+#ifndef LAUNCHER_LAUNCHER_H
+#define LAUNCHER_LAUNCHER_H
+
+#define APPLICATION_MAIN_MODULE L"org.glavo.viewer"
+#define APPLICATION_JRE_PATH L".\\jre"
+
+#endif //LAUNCHER_LAUNCHER_H
